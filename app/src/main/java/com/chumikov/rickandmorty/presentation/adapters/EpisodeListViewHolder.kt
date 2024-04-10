34c1ee3ae63692f81dc022911217853d7d20858d
Aleksandr Chumikov <sashachumikov@rambler.ru>
2024-04-10 15:21:36 +0400

    add_episode_layout_and_adapter

---
+++ app/src/main/java/com/chumikov/rickandmorty/presentation/adapters/EpisodeListViewHolder.kt
@@ -0,0 +1,8 @@
+package com.chumikov.rickandmorty.presentation.adapters
+
+import androidx.recyclerview.widget.RecyclerView
+import com.chumikov.rickandmorty.databinding.ItemEpisodeBinding
+
+class EpisodeListViewHolder(
+    val binding: ItemEpisodeBinding
+) : RecyclerView.ViewHolder(binding.root)

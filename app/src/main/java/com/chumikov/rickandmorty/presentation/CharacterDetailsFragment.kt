27b68ecc5a4a1e5f0765fb4ee1a3d30f375588c3
Aleksandr Chumikov <sashachumikov@rambler.ru>
2024-05-15 19:02:10 +0400

    second_screen_preview_tuning

--- app/src/main/java/com/chumikov/rickandmorty/presentation/CharacterDetailsFragment.kt
+++ app/src/main/java/com/chumikov/rickandmorty/presentation/CharacterDetailsFragment.kt
@@ -8,20 +8,40 @@ import android.view.ViewGroup
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -35,7 +55,13 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.view.isInvisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
@@ -89,31 +115,163 @@ class CharacterDetailsFragment : Fragment() {
             setContent {
                 when (val stateValue = viewModel.status.collectAsState().value) {
                     is CharacterDetailsLoadingState.Success -> {
-                        Box(
+                        Column(
                             modifier = Modifier
                                 .fillMaxSize()
                                 .background(MyBlue)
                         ) {
-                            Box(
+
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = 6.dp),
+                                verticalAlignment = Alignment.CenterVertically,
+                            ) {
+                                IconButton(
+                                    onClick = {
+
+                                    }
+                                ) {
+                                    Icon(
+                                        imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                                        contentDescription = "Arrow Back"
+                                    )
+                                }
+                                Text(
+                                    modifier = Modifier.weight(1f),
+                                    text = "Character's Detailed Info",
+                                    fontWeight = FontWeight.Bold,
+                                    fontFamily = FontFamily.Serif,
+                                    fontSize = 20.sp,
+                                    textAlign = TextAlign.Center
+                                )
+                                Spacer(modifier = Modifier.width(48.dp))
+                            }
+
+                            Column(
                                 modifier = Modifier
                                     .fillMaxSize()
-                                    .padding(30.dp)
-                                    .shadow(
-                                        elevation = 16.dp,
-                                        shape = RoundedCornerShape(12.dp)
+                                    .background(MyBlue)
+                                    .padding(
+                                        start = 14.dp,
+                                        end = 14.dp,
+                                        top = 10.dp,
+                                        bottom = 60.dp
                                     )
+                                    .clip(RoundedCornerShape(12.dp))
                                     .background(MyLightBlue)
                             ) {
-                                Text("Hello World")
+                                val imagePlaceholder = R.drawable.placeholder_image
+                                AsyncImage(
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                        .padding(10.dp)
+                                        .clip(RoundedCornerShape(12.dp))
+                                        .border(
+                                            BorderStroke(3.dp, MyBlue),
+                                            RoundedCornerShape(12.dp)
+                                        )
+                                        .weight(1f),
+                                    model = ImageRequest.Builder(LocalContext.current)
+                                        .data(stateValue.data.imageUrl)
+                                        .crossfade(true)
+                                        .build(),
+                                    placeholder = painterResource(imagePlaceholder),
+                                    error = painterResource(imagePlaceholder),
+                                    contentDescription = "Character's Photo",
+                                    contentScale = ContentScale.Crop,
+                                )
+                                Text(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(10.dp),
+                                    text = String.format(
+                                        getString(R.string.name_template),
+                                        stateValue.data.name
+                                    ),
+                                    fontFamily = FontFamily.Serif,
+                                    fontSize = 18.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    fontStyle = FontStyle.Italic,
+                                    textAlign = TextAlign.Center
+                                )
+                                Text(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(10.dp),
+                                    text = String.format(
+                                        getString(R.string.location_template),
+                                        stateValue.data.location
+                                    ),
+                                    fontFamily = FontFamily.Serif,
+                                    fontSize = 18.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    fontStyle = FontStyle.Italic,
+                                    textAlign = TextAlign.Center
+                                )
+                                Text(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(10.dp),
+                                    text = String.format(
+                                        getString(R.string.species_template),
+                                        stateValue.data.species
+                                    ),
+                                    fontFamily = FontFamily.Serif,
+                                    fontSize = 18.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    fontStyle = FontStyle.Italic,
+                                    textAlign = TextAlign.Center
+                                )
+                                Text(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(10.dp),
+                                    text = String.format(
+                                        getString(R.string.status_template),
+                                        stateValue.data.status
+                                    ),
+                                    fontFamily = FontFamily.Serif,
+                                    fontSize = 18.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    fontStyle = FontStyle.Italic,
+                                    textAlign = TextAlign.Center
+                                )
+                                Button(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(
+                                            top = 10.dp,
+                                            bottom = 26.dp,
+                                            start = 20.dp,
+                                            end = 20.dp
+                                        ),
+                                    onClick = { },
+                                    colors = ButtonDefaults.buttonColors(MyBlue)
+                                ) {
+                                    Text(
+                                        text = "GO TO THE EPISODES INFO",
+                                        fontFamily = FontFamily.Serif,
+                                        fontSize = 18.sp,
+                                        fontWeight = FontWeight.Bold,
+                                    )
+                                }
+
                             }
                         }
                     }
 
                     is CharacterDetailsLoadingState.Loading -> {
-                        Column(
-                            horizontalAlignment = Alignment.CenterHorizontally
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(MyBlue),
+                            contentAlignment = Alignment.Center,
                         ) {
-                            CircularProgressIndicator()
+                            CircularProgressIndicator(
+                                modifier = Modifier.size(80.dp),
+                                color = MyLightBlue
+                            )
                         }
                     }
 
@@ -184,4 +342,41 @@ class CharacterDetailsFragment : Fragment() {
 //        super.onDestroyView()
 //        _binding = null
 //    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun foo() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MyBlue),
+        contentAlignment = Alignment.Center,
+    ) {
+//        TopAppBar(
+//            modifier = Modifier.padding(horizontal = 12.dp),
+//            title = {
+//                Text(
+//                    modifier = Modifier.padding(horizontal = 24.dp),
+//                    text = "Character's Detailed Info",
+//                    fontWeight = FontWeight.Bold,
+//                    fontFamily = FontFamily.Serif,
+//                    fontSize = 20.sp,
+//                    textAlign = TextAlign.Center
+//                )
+//            },
+//            navigationIcon = {
+//                Icon(
+//                    imageVector = Icons.AutoMirrored.Default.ArrowBack,
+//                    contentDescription = "Arrow Back"
+//                )
+//            },
+//            colors = TopAppBarDefaults.topAppBarColors(MyBlue)
+//        )
+
+        CircularProgressIndicator(
+            color = MyLightBlue
+        )
+    }
 }
